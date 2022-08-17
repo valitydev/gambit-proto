26d8c87a309b4e8e3a9413e61eb16a0004696c14
Vasil Khramov <vasil.khramov@gmail.com>
2022-08-17 16:40:53 +0300

    OPS-156: fix after review

--- proto/gambit.thrift
+++ proto/gambit.thrift
@@ -1,13 +1,15 @@
 namespace java dev.vality.gambit
 namespace erlang gambit
 
+typedef i32 Hash
+
 struct DataRequest {
     1: required string dataset_name
-    2: required i32 hash
+    2: required Hash hash
 }
 
 struct IpRequest {
-    1: required i32 hash
+    1: required Hash hash
 }
 
 struct DataResponse {
@@ -18,6 +20,8 @@ struct IpResponse {
     1: required string ip
 }
 
+exception DataSetNotFound {}
+
 /**
 * Интерфейс для получения данных
 */
@@ -26,11 +30,11 @@ service GambitService {
     /**
      * Получение данных из соответствующего источника
      */
-    DataResponse GetData (1: DataRequest data_request)
+    DataResponse GetData (1: DataRequest data_request) throws (1: DataSetNotFound ex);
 
     /**
      * Получение ip адреса
      */
-    IpResponse GetIp (1: IpRequest ip_request)
+    IpResponse GetIp (1: IpRequest ip_request) throws (1: DataSetNotFound ex);
 
 }
