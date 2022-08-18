03078582ff6306d753561835a85922fdd4fb67b2
mr-impossibru <64555470+mr-impossibru@users.noreply.github.com>
2022-08-18 13:05:30 +0300

    OPS-156: init gambit proto (#1)

---
+++ proto/gambit.thrift
@@ -0,0 +1,27 @@
+namespace java dev.vality.gambit
+namespace erlang gambit
+
+typedef i32 Hash
+
+struct DataRequest {
+    1: required list<string> datasets_names
+    2: required Hash hash
+}
+
+struct DataResponse {
+    1: required map<string, string> data
+}
+
+exception DataSetNotFound {}
+
+/**
+* Интерфейс для получения данных
+*/
+service StubDataService {
+
+    /**
+     * Получение данных из нескольких источников
+     */
+    DataResponse GetData (1: DataRequest data_request) throws (1: DataSetNotFound ex);
+
+}
