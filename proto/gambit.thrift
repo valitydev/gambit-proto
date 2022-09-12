931f2b8159d6e317f8552c12c9ac16725cb6162c
mr-impossibru <64555470+mr-impossibru@users.noreply.github.com>
2022-09-12 11:14:58 +0300

    OPS-184: add methods from swag (#6)

--- proto/gambit.thrift
+++ proto/gambit.thrift
@@ -13,10 +13,21 @@ struct DataResponse {
     1: required map<string, string> data
 }
 
+struct DataSetRequest {
+    1: required string data_set_name
+    2: required File file
+}
+
+union File {
+    1: binary csv
+}
+
 exception DataSetNotFound {}
 
+exception DataSetAlreadyExists {}
+
 /**
-* Интерфейс для получения данных
+* Интерфейс для работы с данными
 */
 service StubDataService {
 
@@ -24,5 +35,15 @@ service StubDataService {
      * Получение данных из нескольких источников
      */
     DataResponse GetData (1: DataRequest data_request) throws (1: DataSetNotFound ex);
+
+    /**
+     * Создание набора данных
+     */
+    void CreateDataSet (1: DataSetRequest data_set_request) throws (1: DataSetAlreadyExists ex);
+
+    /**
+     * Добавление записей в существующий набор данных
+     */
+    void UpdateDataSet (1: DataSetRequest data_set_request) throws (1: DataSetNotFound ex);
 
 }
